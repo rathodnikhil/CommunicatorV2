4dffff3d89b85b7dc3f598e2cc778290e5b93c46
vrushali2191 <vrushalis@xanadutec.com>
2018-03-13 21:04:07 +0530

    registration-ui

---
+++ src/app/layout/dashboard/components/default-meeting/default-meeting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-default-meeting',
+  templateUrl: './default-meeting.component.html',
+  styleUrls: ['./default-meeting.component.scss']
+})
+export class DefaultMeetingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
