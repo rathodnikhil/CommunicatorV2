466f150ae5f573995ec8d125c61e43c3cad92714
Nikitesh <nik_0811@yahoo.co.in>
2018-04-03 22:59:35 +0530

    simplertc meeting implementation

--- src/app/layout/blank-page/blank-page.component.ts
+++ src/app/layout/blank-page/blank-page.component.ts
@@ -1,12 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit, Inject, ElementRef } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 @Component({
     selector: 'app-blank-page',
     templateUrl: './blank-page.component.html',
     styleUrls: ['./blank-page.component.scss']
 })
-export class BlankPageComponent implements OnInit {
-    constructor() {}
+export class BlankPageComponent implements OnInit, AfterViewInit {
+    constructor(@Inject(DOCUMENT) private document, private elementRef: ElementRef) { }
+    ngAfterViewInit(): void {
+        // const s = document.createElement('script');
+        // s.type = 'text/javascript';
+        // s.innerHTML = 'console.log(\'done\');'; // inline script
+        // s.src = '../../../assets/scripts/meetingTest.js';
 
-    ngOnInit() {}
+        const s = this.document.createElement('script');
+        s.type = 'text/javascript';
+        s.src = '../../../assets/scripts/meetingTest.js';
+        const __this = this; //to store the current instance to call
+                             //afterScriptAdded function on onload event of
+                             //script.
+        s.onload = function () { __this.afterScriptAdded(); };
+        this.elementRef.nativeElement.appendChild(s);
+    }
+
+
+    ngOnInit() { }
+    afterScriptAdded() {
+        const params = {
+          width: '350px',
+          height: '420px',
+        };
+        if (typeof (window['functionFromExternalScript']) === 'function') {
+          window['functionFromExternalScript'](params);
+        }
+      }
 }
