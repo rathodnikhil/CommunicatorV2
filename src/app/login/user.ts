a58bda7c08934379b537070b6b6cca284a827b16
vrushali2191 <vrushalis@xanadutec.com>
2018-05-15 19:47:27 +0530

    video call ui and routing

--- src/app/login/user.ts
+++
@@ -1,9 +0,0 @@
-import {Role} from '../ModalClass/Role';
-export class User  {
-    private id: Number;
-    private email: String;
-    private name: String;
-    private lastName: String;
-    private password: String;
-    private user_role: Role;
-}
