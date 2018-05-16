af12ed073508e2bd3eb8cf71d3083b59885862c7
Nikitesh <nik_0811@yahoo.co.in>
2018-05-17 02:34:35 +0530

    shared component for modal (#37)

--- src/app/layout/manage-team/manage-team.module.ts
+++ src/app/layout/manage-team/manage-team.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ManageTeamComponent } from './manage-team/manage-team.component';
 import { ManageTeamRoutingModule } from './manage-team-routing.module';
+// import { CustomModalComponent } from '../dashboard/components/custom-modal/custom-modal.component';
+import { DashboardModule } from '../dashboard/dashboard.module';
 
 @NgModule({
   imports: [
-    CommonModule, ManageTeamRoutingModule
+    CommonModule, ManageTeamRoutingModule, DashboardModule
   ],
   declarations: [ManageTeamComponent]
 })
