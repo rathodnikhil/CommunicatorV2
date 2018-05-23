ad58ac59f62e3023b60726947a7028c7857a60f9
Nikitesh <nik_0811@yahoo.co.in>
2018-05-23 23:49:14 +0530

    Newdev (#44)
    * shared component for modal
    
    * video calling half way done
    
    * uncommited code
    
    * few fixes
    
    * changed popup

--- src/app/layout/manage-team/manage-team/manage-team.component.ts
+++ src/app/layout/manage-team/manage-team/manage-team.component.ts
@@ -4,20 +4,20 @@ import { CustomModalComponent, CustomModalModel } from '../../dashboard/componen
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 @Component({
-  selector: 'app-manage-team',
-  templateUrl: './manage-team.component.html',
-  styleUrls: ['./manage-team.component.scss'],
-  providers: [TeamService],
+    selector: 'app-manage-team',
+    templateUrl: './manage-team.component.html',
+    styleUrls: ['./manage-team.component.scss'],
+    providers: [TeamService],
 })
 export class ManageTeamComponent implements OnInit {
-
+    newTeamCreate: any;
     @ViewChild('addNewTeamModal') public addNewTeamModal: CustomModalComponent;
     newTeam: CustomModalModel = {
         titleIcon: '<i class="fa fa-user"></i>',
         title: 'New Team',
         smallHeading: 'You can add new team details here',
         body: '<div class="row"><div class="col-md-3"><label>Team Name</label></div><div><input placeholder="Type Name Here.."/>' +
-              '</div></div>',
+            '</div></div>',
         Button1Content: '<i class="fa fa-user"></i>&nbsp;Add Team',
         Button2Content: ''
     };
@@ -28,52 +28,60 @@ export class ManageTeamComponent implements OnInit {
         title: 'New Member',
         smallHeading: 'You can add new member details here',
         body: '<div class="row"><div class="col-md-3"><label>Member Name</label></div><div><input placeholder="Type Name Here.."/>' +
-              '</div></div>',
+            '</div></div>',
         Button1Content: '<i class="fa fa-user"></i>&nbsp;Add Member',
         Button2Content: ''
     };
 
-_teamService: TeamService;
-userPermissionList = [];
-userPermissionMemList = [];
-selectedTeamName: any;
+    _teamService: TeamService;
+    userPermissionList = [];
+    userPermissionMemList = [];
+    selectedTeamName: any;
 
-  constructor(teamService: TeamService) {
-      this._teamService = teamService;
-   }
+    constructor(teamService: TeamService) {
+        this._teamService = teamService;
+    }
 
-  ngOnInit() {
-      //getTeamsByLoggedInUserId webservice call
-    this.userPermissionList = [];
-    const payload = {loggedInUserId: 1};
-    this._teamService.getTeamsByLoggedInUserId(payload).subscribe(data => {
-        this.userPermissionList = data.json();
-        this.selectedTeamName = this.userPermissionList[1].teamId.teamName;
-    });
+    ngOnInit() {
+        //getTeamsByLoggedInUserId webservice call
+        this.userPermissionList = [];
+        const payload = { loggedInUserId: 1 };
+        this._teamService.getTeamsByLoggedInUserId(payload).subscribe(data => {
+            this.userPermissionList = data.json();
+            this.selectedTeamName = this.userPermissionList[1].teamId.teamName;
+        });
 
-    //getMembersByTeamId webservice call
-this.userPermissionMemList = [];
-const teamPayload = {teamId: 1};
-    this._teamService.getMembersByTeam(teamPayload).subscribe(data => {
-         this.userPermissionMemList = data.json();
-     });
-
-  }
-displayTeamDetails(teamId , teamName) {
-   this.selectedTeamName = teamName;
-    this.userPermissionMemList = [];
-    const teamPayload = {teamId: teamId};
+        //getMembersByTeamId webservice call
+        this.userPermissionMemList = [];
+        const teamPayload = { teamId: 1 };
         this._teamService.getMembersByTeam(teamPayload).subscribe(data => {
-             this.userPermissionMemList = data.json();
-         });
-}
-//to open modal popup
-open() {
-    this.addNewTeamModal.open();
-}
-openMemberPopup() {
-this.addNewMemberModal.open();
-}
+            this.userPermissionMemList = data.json();
+        });
 
-
+    }
+    displayTeamDetails(teamId, teamName) {
+        this.selectedTeamName = teamName;
+        this.userPermissionMemList = [];
+        const teamPayload = { teamId: teamId };
+        this._teamService.getMembersByTeam(teamPayload).subscribe(data => {
+            this.userPermissionMemList = data.json();
+        });
+    }
+    //to open modal popup
+    open() {
+        this.addNewTeamModal.open();
+    }
+    openMemberPopup() {
+        this.addNewMemberModal.open();
+    }
+    CreateTeam(newTeamCreate) {
+        debugger;
+    }
+    close(popupType) {
+        switch (popupType) {
+            case 'AddNewTeam':
+                this.addNewTeamModal.close();
+                break;
+        }
+    }
 }
