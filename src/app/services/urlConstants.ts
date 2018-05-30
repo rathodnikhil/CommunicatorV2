1c3ecba32e0c9b2ed6be318dc0f8ba82dfaa06d8
Nikitesh <nik_0811@yahoo.co.in>
2018-05-31 01:56:19 +0530

    ...

--- src/app/services/urlConstants.ts
+++ src/app/services/urlConstants.ts
@@ -1,2 +1,2 @@
 // export const baseUrl = 'http://192.168.3.74:8080/feedcom/';
-export const baseUrl = 'http://localhost:8081/communicatorV2/';
+export const baseUrl = 'http://192.168.0.105:8081/communicatorV2/';
