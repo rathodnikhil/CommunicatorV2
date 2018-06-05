1f45fb4e360bc72dfe341c230028cf1da241263f
Nikitesh <nik_0811@yahoo.co.in>
2018-06-06 02:45:01 +0530

    added images to background

--- src/app/services/urlConstants.ts
+++ src/app/services/urlConstants.ts
@@ -1,4 +1,4 @@
- export const baseUrl = 'http://localhost:8081/communicatorV2/';
-// export const baseUrl = 'http://192.168.0.106:8081/communicatorV2/';
-//export const baseUrl = 'http://192.168.0.104:8081/communicatorV2/';
+//  export const baseUrl = 'http://localhost:8081/communicatorV2/';
+export const baseUrl = 'http://192.168.0.106:8081/communicatorV2/';
+// export const baseUrl = 'http://192.168.0.104:8081/communicatorV2/';
 export const rsaPublicKey = 'dmsxxhetzejiibz6z99xnjenpxzk8e9uu8kpj42b93gaht0od0or634dnssz93wsngrjt4lwpmqijlgf0jzb2duv9wkprq73lh11xgn84jjd1rqcfdcllxk0e09ln4fw3t47tsgggbdjp7zm15uwx9zv3u6k7h4y1y9mk9t27dken9q0umnodpcofaak5p7lhmnlenkhg1ua6gwm16rnz5wd6glpme9thcj1g9r620sf91h9z5no5xz6dc1';
