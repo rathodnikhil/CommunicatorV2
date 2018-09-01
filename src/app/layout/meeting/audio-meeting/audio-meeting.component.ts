33ddf03c827c1ceb3a3b5a6cb4ddefec1e41efe2
XTDOMAIN\vrushalis <vrushalis@xanadutec.com>
2018-09-01 17:57:52 +0530

    hell lot of changes

---
+++ src/app/layout/meeting/audio-meeting/audio-meeting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-audio-meeting',
+  templateUrl: './audio-meeting.component.html',
+  styleUrls: ['./audio-meeting.component.scss']
+})
+export class AudioMeetingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
