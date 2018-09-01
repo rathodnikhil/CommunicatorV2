59437b085969578f42e1ff981103b3eccd0e7aec
XTDOMAIN\vrushalis <vrushalis@xanadutec.com>
2018-09-01 21:13:03 +0530

    kela ekdacha audio meeting

--- src/app/layout/manage-group/manage-group.module.ts
+++ src/app/layout/manage-group/manage-group.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 import { ManageGroupComponent } from './manage-group/manage-group.component';
 import { ManageGroupRoutingModule } from './manage-group.routing.module';
 import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
-// import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { SharedPipesModule } from '../../shared';
 
 @NgModule({
   imports: [
     CommonModule, 
     ManageGroupRoutingModule, 
-    NgMultiSelectDropDownModule.forRoot()
-    // FormsModule
+    NgMultiSelectDropDownModule.forRoot(),
+    FormsModule,
+    NgxPaginationModule,
+    SharedPipesModule
   ],
   declarations: [ManageGroupComponent]
 })
