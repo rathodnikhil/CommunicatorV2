e297185597e889ba6dfe1c36522bc114f1ab3d79
XTDOMAIN\vrushalis <vrushalis@xanadutec.com>
2019-01-18 19:31:56 +0530

    guest user mapping

--- src/app/reset-password/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password/reset-password.component.ts
@@ -52,9 +52,9 @@ export class ResetPasswordComponent implements OnInit {
         } else {
           this.password = '';
           this.confirmPassword = '';
-          this.router.navigate(['/login']);
+        //  this.router.navigate(['/login']);
+        this.alertService.success('Password reset successfully', 'Reset Success');
           window.location.reload();
-          return this.alertService.success('Password reset successfully', 'Reset Success');
         }
       });
     }
