bd8a8402216285fd99c42de76278b74e53dead5c
XTDOMAIN\vrushalis <vrushalis@xanadutec.com>
2019-03-16 19:01:21 +0530

    removed unused components

--- src/app/layout/meeting/meeting-routing.module.ts
+++ src/app/layout/meeting/meeting-routing.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MeetingComponent } from './meeting/meeting.component';
-import { AudioMeetingComponent } from './audio-meeting/audio-meeting.component';
-
 
 const routes: Routes = [
     {
         path: '',
         component: MeetingComponent
-    },
-    {
-        path: 'audio',
-        component: AudioMeetingComponent
     }
 ];
 
