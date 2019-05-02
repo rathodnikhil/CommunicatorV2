abd39dc7f88d004cfc371037c2203723a0e3637f
vrushali2191 <34684429+vrushali2191@users.noreply.github.com>
2019-05-02 17:28:49 +0530

    My vr (#332)
    * schedule meeting remove commented coe
    
    * localgroup by member payload changes
    
    * menu validation and few other bugs
    
    * bugs
    
    * spelling corrections
    
    * few bugs
    
    * changes for join meeting
    
    * spinner and footer basics
    
    * changes for commit
    
    * past meeting by month and loadmore
    
    * redesign
    
    * default meeting redesign and notification
    
    * default meeting redesign all most done
    
    * dashboard redesign and dynamic base url
    
    * loading code unused remove
    
    * dynamic meeting url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl : 'https://cfscommunicator.com:8443/prod_communicatorv2/'                    // for production server
+  baseUrl : 'https://cfscommunicator.com:8443/prod_communicatorv2/',                // for production server
  // baseUrl : 'https://cfscommunicator.com:8443/test_communicatorv2/'                   // for test  server
+  meetingUrl: 'https://cfscommunicator.com/'
 };
