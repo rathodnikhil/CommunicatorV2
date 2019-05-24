f6c63a4b7f8c81c13fb271423fdd739cc07df4ae
vrushali2191 <34684429+vrushali2191@users.noreply.github.com>
2019-05-24 12:33:07 +0530

    Refactor vr (#382)
    * schedule meeting remove commented coe
    
    * localgroup by member payload changes
    
    * menu validation and few other bugs
    
    * bugs
    
    * spelling corrections
    
    * few bugs
    
    * changes for join meeting
    
    * spinner and footer basics
    
    * changes for commit
    
    * past meeting by month and loadmore
    
    * redesign
    
    * default meeting redesign and notification
    
    * default meeting redesign all most done
    
    * dashboard redesign and dynamic base url
    
    * loading code unused remove
    
    * dynamic meeting url
    
    * code refactoring for menu in progress
    
    * code refactoring in process
    
    * meeting modify ui integration
    
    * changes in login, register-admin and notification
    
    * bugs and meeting ui integration
    
    * error mesage constant in progress
    
    * meeting color reset
    
    * bugs and meeting color reset
    
    * code refactor
    
    * file count and tab swich on icon bar click"
    
    * code optimization in process

--- src/assets/scripts/meetingTest.js
+++ src/assets/scripts/meetingTest.js
@@ -438,8 +438,8 @@ connection.onstream = function (event) {
         customDiv.setAttribute("style" , "  background-color: #ffeeed; margin-bottom: 15px; height:20vh; position: relative; border: 1px solid #AF2127; color:#AF2127;");
         heading.setAttribute("style", " background-color:#AF2127;padding: 1.3%;text-align: center;  font-weight: bold; color:#ffffff;");
     } else {
+        customDiv.setAttribute("style" , "background-color: #e1f3ff; margin-bottom: 15px; height:20vh; position: relative; border: 1px solid #2199e8; color:#2199e8;");
         if (event.stream.isVideo == 0) {
-            customDiv.setAttribute("style" , "background-color: #e1f3ff; margin-bottom: 15px; height:20vh; position: relative; border: 1px solid #2199e8; color:#2199e8;");
             heading.setAttribute("style", "background-color:#2199e8;;padding: 1.3%;text-align: center;  font-weight: bold; color:#ffffff;");
         } else {
             heading.setAttribute("style", ' padding: 1.3%;  text-align: center;');
