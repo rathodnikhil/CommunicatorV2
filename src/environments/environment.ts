1c3c3d5d93192e7aa11f04ea5775a1a98436c01d
vrushali2191 <34684429+vrushali2191@users.noreply.github.com>
2019-06-13 18:37:14 +0530

    Refactor (#411)
    * schedule meeting remove commented coe
    
    * localgroup by member payload changes
    
    * menu validation and few other bugs
    
    * bugs
    
    * spelling corrections
    
    * few bugs
    
    * changes for join meeting
    
    * spinner and footer basics
    
    * changes for commit
    
    * past meeting by month and loadmore
    
    * redesign
    
    * default meeting redesign and notification
    
    * default meeting redesign all most done
    
    * dashboard redesign and dynamic base url
    
    * loading code unused remove
    
    * dynamic meeting url
    
    * exit meeting flow
    
    * meeting exit flow in progress
    
    * meeting exit flow in progress
    
    * gareebi andolan
    
    * meeting and other changes
    
    * remove notification
    
    * few change
    
    * few changes
    
    * margin to logo on login type

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@
 
 export const environment = {
       production: false,
-     baseUrl: 'http://localhost:8080/communicatorV2/',
-  //   baseUrl: 'https://cfscommunicator.com:8443/test_communicatorv2/',
+    // baseUrl: 'http://localhost:8080/communicatorV2/',
+     baseUrl: 'https://cfscommunicator.com:8443/test_communicatorv2/'
 };
